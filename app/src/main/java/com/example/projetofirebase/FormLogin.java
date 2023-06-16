bd14ec1b1737579340cee58969636276b68f00bd
Sophia Wermann <sophiawermann@gmail.com>
2023-06-16 10:39:57 -0300

    user authentication and database config

--- app/src/main/java/com/example/projetofirebase/FormLogin.java
+++ app/src/main/java/com/example/projetofirebase/FormLogin.java
@@ -3,13 +3,29 @@ package com.example.projetofirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView text_tela_cadastro;
+    private EditText edit_email, edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressbar;
+    String[] mensagens = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +42,84 @@ public class FormLogin extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if(email.isEmpty() || senha.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                } else {
+                    autenticarUsuario(v);
+                }
+            }
+        });
     }
 
     private void iniciarComponentes(){
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progressbar = findViewById(R.id.progressbar);
     }
+
+    private void autenticarUsuario(View v) {
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+
+                if(task.isSuccessful()) {
+                    progressbar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            telaPrincipal();
+                        }
+                    }, 3000);
+
+                } else {
+                    String erro;
+                    try {
+                        throw task.getException();
+                    } catch (Exception e) {
+                        erro = "Erro ao logar usuário";
+                    }
+
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(usuarioAtual != null) {
+          telaPrincipal();
+        }
+    }
+
+    private void telaPrincipal() {
+        Intent intent = new Intent(FormLogin.this, TelaPrincipal.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
